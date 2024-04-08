47b02f3784dd2e759361f66a1fa113f278a754b7
agnibha-chatterjee <agni.bhaaa@gmail.com>
2024-04-08 01:21:16 -0700

    implemented all auth routes and logic

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 // const express= require('express');
+import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
 import Hello from "./Hello.js";
@@ -6,12 +7,41 @@ import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from "./Kanbas/assignments/router.js";
+import UserRoutes from "./Users/routes.js";
 import cors from "cors";
+import session from "express-session";
+import mongoose from "mongoose";
+
+mongoose.connect(
+  "mongodb+srv://chatterjeeag:bMRyknHel0fqYuw4@master-cluster.tzwazee.mongodb.net/kanbas?retryWrites=true&w=majority&appName=master-cluster"
+);
+
 const app = express();
 
 app.use(morgan("dev"));
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+
+const sessionOptions = {
+  secret: "keyboardcat",
+  resave: false,
+  saveUninitialized: true,
+};
+
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+
 app.use(express.json());
+app.use(session(sessionOptions));
 
 app.get("/", (_, res) => {
   res.send({ status: "ok" });
@@ -22,6 +52,7 @@ CourseRoutes(app);
 AssignmentRoutes(app);
 Lab5(app);
 Hello(app);
+UserRoutes(app);
 
 const PORT = process.env.PORT || 4000;
 
