f9822e0b26d43158dcaba83d4732fcc74820ea44
agnibha-chatterjee <agni.bhaaa@gmail.com>
2024-04-08 02:48:20 -0700

    implemented CRUD for modules and courses

--- Users/dao.js
+++ Users/dao.js
@@ -13,3 +13,4 @@ export const findUserByCredentials = (username, password) =>
 export const updateUser = (userId, user) =>
   model.updateOne({ _id: userId }, { $set: user });
 export const deleteUser = (userId) => model.deleteOne({ _id: userId });
+export const findUsersByRole = (role) => model.find({ role: role });
